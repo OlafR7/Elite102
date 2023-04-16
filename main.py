11817a71072c69d17b2d682889e9cc3b962f6086
Olaf Rich <olafrich23@icloud.com>
2023-04-16 16:24:18 -0500

    Fixed file issues & started account functions

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import mysql.connector
-from account_access import create_account
-from account_access import login
+from account_access import *
 
-
-connection = mysql.connector.connect(user = 'root',database = 'example',password = 'Cjrules1106*')
-cursor = connection.cursor()
+in_account = False
+name = ""
+pin = 0
+balance = 0
 while True:
-    response = input("Would you like to log in or sign up(l or s or quit): ")
+    response = input("Would you like to log in, sign up, or quit(l or s or quit): ")
     if response == "quit":
         print("Thank you for using our product.")
         break
     elif response == "s":
         create_account()
     elif response == "l":
-        login()
+        name, pin, balance = login()
+        print(f'Name: {name}\nPin Number: {pin}\nBalance: {balance}')
+        while True:
+            next_response = input("Would you like to deposit money, withdrawl money, delete your account, or log out(deposit, withdrawl, delete, leave): ")
+            next_response = next_response.lower
+            if(next_response == 'leave'):
+                print("Successfully logged out.")
+                break
+            elif next_response == 'deposit':
+                print('not done')
+            elif next_response == 'withdrawl':
+                print('not done')
+            elif next_response == 'delete':
+                print('not done')
+            
+
+        
+        
