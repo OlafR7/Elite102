a25ee2e6125dda6ee2686090c29d8e331148b48a
Olaf Rich <olafrich23@icloud.com>
2023-04-16 16:25:44 -0500

    fixed issues

--- account_access.py
+++ account_access.py
@@ -12,6 +12,8 @@ def create_account():
 
     cursor.execute(add_account)
     connection.commit()
+    cursor.close()
+    connection.close()
 
 def login():
     connection = mysql.connector.connect(user = 'root', database = 'example', password = 'Cjrules1106*')
@@ -19,6 +21,10 @@ def login():
 
     name = input("Enter your name: ")
     pin = input("Enter your pin: ")
-    get_account = (f'SELECT  * accounts WHERE name = \"{name}\" AND pin = {pin}')
- 
-    
+    get_account = (f'SELECT name, pin, balance FROM accounts WHERE name = \"{name}\" AND pin = {pin}')
+    cursor.execute(get_account)
+    for item in cursor:
+        return(item[0], item[1], item[2])
+            
+    cursor.close()
+    connection.close()
