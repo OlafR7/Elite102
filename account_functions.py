b60a8157aee85a9de55ae9dfc1fa29a0224b2faa
Olaf Rich <olafrich23@icloud.com>
2023-04-24 21:04:41 -0500

    Added Error handling and fixed CLI formatting

--- account_functions.py
+++ account_functions.py
@@ -3,15 +3,20 @@ import math
 
 def deposit(id, bal):
     amount = input("How much would you like to deposit(No special characters): ")
-    amount = float(amount)
+    try:
+        amount = float(amount)
+    except:
+        print("Sorry, that isn't a number.")
+        return bal
     amount = math.floor(amount * 100)/100
-    bal = float(bal) + amount
+    bal = round(float(bal) + amount, 2)
     
     connection = mysql.connector.connect(user = 'user', database = 'example', password = 'Pass!word')
     cursor = connection.cursor()
     
     add_money = (f'UPDATE accounts SET balance = {bal} WHERE id = {id}')
     cursor.execute(add_money)
+    print(f'Sucessfully deposited ${amount}.')
     
     connection.commit()
     cursor.close()
@@ -20,18 +25,23 @@ def deposit(id, bal):
 
 def withdrawl(id, bal):
     amount = input("How much would you like to withdrawl(No special characters): ")
-    amount = float(amount)
+    try:
+        amount = float(amount)
+    except:
+        print("Sorry, that isn't a number.")
+        return bal
     amount = math.floor(amount * 100)/100
     if float(bal) - amount < 0:
         print("You don't have that much money in your account.")
         return bal
-    bal = float(bal) - amount
+    bal = round(float(bal) - amount, 2)
 
     connection = mysql.connector.connect(user = 'user', database = 'example', password = 'Pass!word')
     cursor = connection.cursor()
 
     take_money = (f'UPDATE accounts SET balance = {bal} WHERE id = {id}')
     cursor.execute(take_money)
+    print(f'Sucessfully withdrawled ${amount}.')
 
     connection.commit()
     cursor.close()
@@ -44,6 +54,7 @@ def delete(id):
 
     remove_account = (f'DELETE FROM accounts WHERE id = {id}')
     cursor.execute(remove_account)
+    print("Account successfully removed.")
 
     connection.commit()
     cursor.close()
--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from account_access import *
 from account_functions import *
 
+using_program = True
+in_account = False
 account_num = 0
 name = ""
 pin = 0
 balance = 0
-while True:
+while using_program:
     response = input("Would you like to log in, sign up, or quit(l or s or quit): ")
+    print()
     if response == "quit":
         print("Thank you for using our product.")
-        break
+        using_program = False
     elif response == "s":
         create_account()
+        print()
     elif response == "l":
-        account_num, name, pin, balance = login()
-        print(f'Account_num: {account_num}\nName: {name}\nPin Number: {pin}\nBalance: {balance}')
-        while True:
+        try:
+            account_num, name, pin, balance = login()
+            print()
+            print(f'Account number: {account_num}\nName: {name}\nPin Number: {pin}\nBalance: {balance}')
+            print()
+            in_account = True
+        except:
+            print("Sorry, that account doesn't exist.\n")
+        while in_account:
             next_response = input("Would you like to deposit money, withdrawl money, check your balance, delete your account, or log out(deposit, withdrawl, balance, delete, leave): ")
+            print()
             next_response = next_response.lower()
             if(next_response == 'leave'):
                 account_num = 0
@@ -24,22 +35,27 @@ while True:
                 pin = 0
                 balance = 0
                 print("Successfully logged out.")
-                break
+                print()
+                in_account = False
             elif next_response == 'deposit':
                 new_bal = deposit(account_num, balance)
+                print()
                 balance = new_bal
             elif next_response == 'withdrawl':
                 new_bal = withdrawl(account_num, balance)
+                print()
                 balance = new_bal
             elif next_response == 'balance':
-                print(f"Balance: {balance}")
+                print(f"Balance: ${balance}")
+                print()
             elif next_response == 'delete':
                 delete(account_num)
+                print()
                 account_num = 0
                 name = ""
                 pin = 0
                 balance = 0
-                break
+                in_account = False
             
 
         
